5c33b31d0d8c417c84555b6c992dcd374668d419
Md. Sishir Rahman Siam <162951877+shishirRsiam@users.noreply.github.com>
2024-06-16 21:59:02 +0600

    Add files via upload
    update

--- Practice Question 1/Solve.sql
+++ Practice Question 1/Solve.sql
@@ -2,15 +2,15 @@ create database if not exists XYZ_Company;
 
 use XYZ_Company;
 
-create table DB (
+create table employee (
 	id int primary key,
 	name varchar(50),
 	salary int
 );
 
-insert into DB (id, name, salary) values
+insert into employee (id, name, salary) values
 	(1, "adam", 25000),
 	(2, "bob", 30000),
 	(3, "casey", 40000);
 
-select * from DB;
+select * from employee;
