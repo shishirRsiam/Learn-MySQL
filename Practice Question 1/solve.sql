f2737ad8632c00dd3bc85dbf21283258dfbfddfc
Md. Sishir Rahman Siam <162951877+shishirRsiam@users.noreply.github.com>
2024-06-16 21:21:23 +0600

    Update solve.sql

--- Practice Question 1/solve.sql
+++ Practice Question 1/solve.sql
@@ -2,13 +2,13 @@ create database if not exists XYZ_Company;
 
 use XYZ_Company;
 
-create table DB(
-	name varchar(50),
-    salary int);
+create table DB
+	(name varchar(50),
+	salary int);
 
 insert into DB (name, salary) values
-("adam", 25000),
-("bob", 30000),
-("casey", 400000);
+	("adam", 25000),
+	("bob", 30000),
+	("casey", 400000);
 
 select * from DB;
